4cf3976051eef4de18b745ecd3da2ff93260169b
Krzysztof Blicharz <19705@student.ans-elblag.pl>
2024-01-22 13:05:54 +0100

    ConverterView in progress

---
+++ MVVM/ViewModels/ConverterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitsNet;
+
+namespace MAUIVERTER.MVVM.ViewModels
+{
+    public class ConverterViewModel
+    {
+        public string QuantityName { get; set; }
+        public ObservableCollection<string> FromMeasures { get; set; }
+        public ObservableCollection<string> ToMeasures { get; set; }
+
+        private ObservableCollection<string> LoadMeasures()
+        {
+            var types = Quantity.Infos.FirstOrDefault(x => x.Name == QuantityName).UnitInfos;
+        }
+    }
+}
