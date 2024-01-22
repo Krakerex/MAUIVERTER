51d437b7965a4f1762527452823838ec66257d50
Krzysztof Blicharz <19705@student.ans-elblag.pl>
2024-01-22 09:00:29 +0100

    Added menu

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,10 +13,11 @@ namespace MAUIVERTER
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("fontello.ttf", "Icons");
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
